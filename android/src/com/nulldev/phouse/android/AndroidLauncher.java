b218828043e2f8af14a4e6001cbcb40721505b9a
BuildTools <andydominationhaha>
2015-01-24 14:34:30 -0500

    Add basic project files...

---
+++ android/src/com/nulldev/phouse/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nulldev.phouse.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nulldev.phouse.Phouse;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Phouse(), config);
+	}
+}
